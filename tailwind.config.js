ca829cc04f8a4c75900ecddd6334dd5c5bee435d
ac-89 <alancarroll89@gmail.com>
2023-06-30 11:12:27 +0100

    initail commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./css/*.css", "./assets/**.*.{css, html, js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
